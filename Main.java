342378c10b8302edf14641f5c87590aa6b286231
DavidM <169084424+codeby-david@users.noreply.github.com>
2025-01-27 18:20:56 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -49,3 +49,65 @@ public class Main{
 	
 	
 }
+
+
+
+//USING SWITCH CASE
+
+
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner myObject= new Scanner(System.in);
+		
+		char operators[]={'+','-','*','/'};
+
+		
+		
+		 
+		 double firstNumber=0;
+		 double secondNumber=0;
+		 System.out.println("Enter the first number");
+		 firstNumber=myObject.nextDouble();
+		 System.out.println("Enter the operator");
+		 char operator=myObject.next().charAt(0);
+		 System.out.println("Enter the second number");
+		 secondNumber=myObject.nextDouble();
+		switch (operator) {
+		case '+':
+			
+			System.out.printf("%.2f + %.2f = %.2f",firstNumber,secondNumber,firstNumber+secondNumber);
+			break;
+		
+		case '-':
+			System.out.printf("%.2f - %.2f = %.2f",firstNumber,secondNumber,firstNumber-secondNumber);
+			break;
+		case '*':
+			System.out.printf("%.2f * %.2f = %.2f",firstNumber,secondNumber,firstNumber*secondNumber);
+			break;
+		case '/':
+			if(secondNumber==0) {
+				System.out.printf("Cannot divide by zero");
+			}
+			else {
+			System.out.printf("%.2f / %.2f = %.2f",firstNumber,secondNumber,firstNumber/secondNumber);
+			}
+			break;
+		default:
+			System.out.println("Invalid operator");
+			
+		}
+		myObject.close();
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
